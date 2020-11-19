60c8ed25a61e7e96fe3c12e3dcc502e0e53d7284
HeeChan-Yang <playonmarce@gmail.com>
2020-11-19 23:53:29 +0900

    test

--- StoreRandomGenerator/js/main.js
+++ StoreRandomGenerator/js/main.js
@@ -1,5 +1,15 @@
 const randomBtn = document.getElementById("randomBtn");
+const storeName = document.getElementById("storeName");
+const storeAddress = document.getElementById("storeAddress");
+let storeData;
+
+$.getJSON('https://heechan-yang.github.io/study/store.json', (data) => {
+    storeData = data;
+})
 
 randomBtn.onclick = () => {
-    console.log('test');
+    let storeCount = storeData.storeData.length
+    let randCount = Math.floor(Math.random()*storeCount);
+    storeName.innerText = storeData.storeData[randCount].storeName;
+    storeAddress.innerText = storeData.storeData[randCount].storeAddress;
 }
